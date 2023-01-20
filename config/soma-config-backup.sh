19ff5cfdef735fccac1d03ad5b5373c965ab0dde
Adrian Schmid <83641875+Moondog8862@users.noreply.github.com>
2023-01-20 20:30:40 +0100

    Add files via upload

---
+++ config/soma-config-backup.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+#####################
+# SOMA Backup
+#####################
+CP=/bin/cp
+
+DATE=$(/bin/date +%Y-%m-%d-week%w)
+
+# soma-script-and-backup-folder
+SOMA_BASE=/var/lib/soma
+
+# Backup configs
+cd $SOMA_BASE/config
+
+# Print current routing table as systeminfo
+printf %s\\n "$(route -n | column -t -s "|" )"  > system-setup/$DATE-routing-table.txt
+
+$CP /etc/hostapd/hostapd.conf hostapd/
+$CP /etc/netplan/01-netcfg.yaml netplan/
+$CP /etc/dnsmasq.conf dnsmasq/
+$CP /etc/ufw/before.rules ufw/
+$CP /etc/ufw/user.rules ufw/
+$CP /etc/ufw/sysctl.conf ufw/
+$CP /etc/default/ufw ufw/
+$CP /etc/samba/smb.conf smb/ 
+$CP /etc/init.d/tvheadend/ tvheadend/
+$CP /etc/exports nfs/
+$CP $SOMA_BASE/somastart ./
+$CP /etc/sysctl.conf ./
+$CP /etc/udev/rules.d/90-dvb-adapter.rules ./
+
+# ONLY BACKUP
+$CP /etc/ssh/sshd_config ./
+$CP /etc/resolv.conf ./
+$CP /etc/hosts ./
+$CP /etc/rc.local ./
+$CP /etc/iptables/rules.v4 ./
+rsync -rvpog /etc/modprobe.d ./
+# cp /etc/default/lxc default/
+
